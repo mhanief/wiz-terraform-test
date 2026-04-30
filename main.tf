997bcd7a08f9349e867b7eef0eefe7a9dd1c79fa
mhanief <47133144+mhanief@users.noreply.github.com>
2026-04-30 22:49:28 +0800

    Add subscription_id to main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ provider "azurerm" {
 
   skip_provider_registration = true
   use_cli                    = false
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
 }
 
 resource "azurerm_resource_group" "wiz_test_rg" {
